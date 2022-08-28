286cbe89cadf15f3ca66562d69db91e7b7a90454
SchasembackAxel <schasemback_axel@hotmail.com>
2022-08-27 23:03:01 -0300

    Agregamos json y mostramos en html

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react'
 import Navbar from './components/Navbar/Navbar';
 import Search from './components/Search/Search';
-import { ItemListContainer } from './components/ItemListContainer/ItemList';
+import {ItemListContainer} from './components/ItemListContainer/ItemList';
 
 function App() {
   return (
@@ -10,12 +10,9 @@ function App() {
       <Navbar />
       <main className='mainContainer'>
         <Search searchProd="Buscar Producto" />
-        <ItemListContainer
-          title="Item List Container"
-          img="https://celularapagos.com/wp-content/uploads/2022/04/moto-G71-azul.png"
-          titleCard="Moto G71"
-          price="$92000"
-        />
+        <div className='divContent'>
+        <ItemListContainer/>
+        </div>
       </main>
     </div>
   )
--- src/components/ItemListContainer/ItemList.jsx
+++ src/components/ItemListContainer/ItemList.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import "./ItemList.css"
 import { ItemCount } from '../ItemCount/ItemCount'
 
 
+export const ItemListContainer = () => {
+    const [productos, setProductos] = useState([]);
 
-export const ItemListContainer = ({ title, img, titleCard, price }) => {
-    const addCart = (count) => {  
-        console.log(`agregaste: \n nombre: ${titleCard} \n precio: ${price} \n cantidad: ${count}`)
+
+    useEffect(() => {
+        fetch('./json/data.JSON')
+            .then((response) => response.json())
+            .then((data => {
+                data = data.productos
+
+                const productosJSX = data.map((item) =>
+                    < div key={item.id} className='cardContent' >
+                        <div className='allCards'>
+                            <div className='card'>
+                                <img src={item.img} alt="imagenPrueba" className="imgCard" />
+                                <span className='titleCard'>{item.marca} {item.nombre} </span>
+                                <span className="priceCard">${item.precio}</span>
+                                <ItemCount stock={item.stock} addCart={(count) => {
+                                    console.log(`agregaste: \n marca: ${item.marca} \n nombre: ${item.nombre} \n precio: ${item.precio} \n cantidad: ${count}`)
+                                }} />
+                            </div>
+                        </div>
+                    </div >)
+                setProductos(productosJSX)
+            }))
+            .catch((error) => {
+                console.error(error)
+            })
+    }, [])
+    function consultPromise(confirm) {
+        return new Promise((res, rej) => {
+            if (confirm) {
+                res(productos)
+            } else {
+                rej("reject")
+            }
+
+        })
     }
     return (
-        <div className='cardContent'>
-            <h1 className='title'> {title} </h1>
-            <div className='allCards'>
-                <div className='card'>
-                    <img src={img} alt="imagenPrueba" className="imgCard" />
-                    <span className='titleCard'> {titleCard} </span>
-                    <span className="priceCard">{price}</span>
-                    <ItemCount stock={7} addCart={addCart}/>
-                </div>
-            </div>
-        </div>
+        <>
+            {consultPromise(true) && productos}
+        </>
     )
 }
